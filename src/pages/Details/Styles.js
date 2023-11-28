d6895c1023ce56086f9b31951c1cdbb551e5f01e
KennzyCardoso <kennzy418@gmail.com>
2023-11-28 05:57:06 -0300

    Adiciona transição suave para a mudança de imagem de fundo

--- src/pages/Details/Styles.js
+++ src/pages/Details/Styles.js
@@ -75,6 +75,33 @@ export const ContainerSecond = styled.div`
   align-items: center;
   height: 100vh;
   text-align: center;
+
+  h2, p {
+    position: relative; /* Adicione isso para garantir que o z-index funcione */
+    z-index: 1;
+    color: white;
+  }
+
+  h2 {
+    line-height: 2;
+    font-size: 50px;
+  }
+
+  p {
+    line-height: 2;
+    font-size: 25px;
+  }
+
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.3);
+    z-index: 0;
+  }
 `;
 export const ContainerThree = styled.div`
 position: relative;
@@ -87,20 +114,35 @@ position: relative;
   align-items: center;
   height: 100vh;
   text-align: center;
+
+  h2, p {
+    position: relative; /* Adicione isso para garantir que o z-index funcione */
+    z-index: 1;
+    color: white;
+  }
+
+  h2 {
+    line-height: 2;
+    font-size: 50px;
+  }
+
+  p {
+    line-height: 2;
+    font-size: 25px;
+  }
+
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.3);
+    z-index: 0;
+  }
 `;
 export const ContainerFor = styled.div`
-position: relative;
-  background: url(${backgroundTratamentoDeLesões}) no-repeat center center;
-  background-size: cover;
-  background-position: center;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  text-align: center;
-`;
-export const ContainerFive = styled.div`
 position: relative;
   background: url(${backgroundPilates}) no-repeat center center;
   background-size: cover;
@@ -111,4 +153,31 @@ position: relative;
   align-items: center;
   height: 100vh;
   text-align: center;
+
+  h2, p {
+    position: relative; /* Adicione isso para garantir que o z-index funcione */
+    z-index: 1;
+    color: white;
+  }
+
+  h2 {
+    line-height: 2;
+    font-size: 50px;
+  }
+
+  p {
+    line-height: 2;
+    font-size: 25px;
+  }
+
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.3);
+    z-index: 0;
+  }
 `;
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ContainerFirst, ContainerSecond, ContainerThree, ContainerFor, ContainerFive } from './Styles';
+import { ContainerFirst, ContainerSecond, ContainerThree, ContainerFor } from './Styles';
 
 export function Details() {
   return (
@@ -11,26 +11,17 @@ export function Details() {
       </ContainerFirst>
 
       <ContainerSecond>
-        <h2>OLÁ! SOU A DRA. JULIANA SILVA, <br/> MUITO PRAZER</h2>
-        <p>APAIXONADA POR FISIOTERAPIA GERAL<br/>REABILITAÇÃO MUSCULAR<br/>TRATAMENTO DE LESÕES<br/>PILATES</p>
-        <button onClick={() => window.location.href = 'https://wa.me/seuNumeroDeTelefone'}>Contato</button>
+        <h2>Reabilitação Muscular: Caminho para o Bem-Estar</h2>
+        <p>A reabilitação muscular é um processo personalizado e dedicado à restauração da função muscular, visando melhorar a qualidade de vida dos pacientes. A Dra. Juliana é especializada em utilizar abordagens inovadoras, combinando terapias manuais, exercícios específicos e tecnologia avançada para promover a cura e fortalecimento muscular.</p>
       </ContainerSecond>
       <ContainerThree>
-        <h2>OLÁ! SOU A DRA. JULIANA SILVA, <br/> MUITO PRAZER</h2>
-        <p>APAIXONADA POR FISIOTERAPIA GERAL<br/>REABILITAÇÃO MUSCULAR<br/>TRATAMENTO DE LESÕES<br/>PILATES</p>
-        <button onClick={() => window.location.href = 'https://wa.me/seuNumeroDeTelefone'}>Contato</button>
+        <h2>Tratamento de Lesões: Recuperação Personalizada para Sua Vitalidade</h2>
+        <p>Na nossa clínica, entendemos que as lesões podem impactar profundamente a qualidade de vida. Oferecemos um tratamento dedicado e personalizado, guiando-o passo a passo em direção à recuperação completa. Nosso foco no tratamento de lesões vai além da simples cura; buscamos restaurar sua força, mobilidade e confiança.</p>
       </ContainerThree>
       <ContainerFor>
-        <h2>OLÁ! SOU A DRA. JULIANA SILVA, <br/> MUITO PRAZER</h2>
-        <p>APAIXONADA POR FISIOTERAPIA GERAL<br/>REABILITAÇÃO MUSCULAR<br/>TRATAMENTO DE LESÕES<br/>PILATES</p>
-        <button onClick={() => window.location.href = 'https://wa.me/seuNumeroDeTelefone'}>Contato</button>
+        <h2>Descubra o Pilates: Transforme Seu Corpo, Fortaleça Sua Mente</h2>
+        <p>Bem-vindo à nossa jornada de Pilates, onde a transformação física e mental se entrelaçam para criar uma nova versão mais forte, flexível e equilibrada de você mesmo. O Pilates é mais do que um exercício; é uma abordagem holística que fortalece não apenas seu corpo, mas também sua mente. Descubra como nosso programa de Pilates pode elevar sua qualidade de vida.</p>
       </ContainerFor>
-      <ContainerFive>
-        <h2>OLÁ! SOU A DRA. JULIANA SILVA, <br/> MUITO PRAZER</h2>
-        <p>APAIXONADA POR FISIOTERAPIA GERAL<br/>REABILITAÇÃO MUSCULAR<br/>TRATAMENTO DE LESÕES<br/>PILATES</p>
-        <button onClick={() => window.location.href = 'https://wa.me/seuNumeroDeTelefone'}>Contato</button>
-      </ContainerFive>
-      
     </div>
   );
 }
