56db5f9a85537e44754d8a0bd84affad6d5d173a
KennzyCardoso <kennzy418@gmail.com>
2023-12-04 23:10:56 -0300

    Refatoração do componente principal

---
+++ src/components/container.js
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  position: relative;
+  background: url(${props => props.backgroundImage}) no-repeat center center;
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  text-align: center;
+
+  h2, p {
+    position: relative;
+    z-index: 1;
+    color: white;
+    line-height: 2;
+    font-size: 2rem;
+  }
+
+  p {
+    font-size: 1.5rem;
+  }
+
+  @media (max-width: 768px) {
+    h2 {
+      font-size: 1.5rem;
+    }
+
+    p {
+      font-size: 1rem;
+    }
+  }
+
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.3);
+    z-index: 0;
+  }
+
+  button {
+    color: white;
+    font-size: 18px;
+    padding: 10px 20px;
+    border: 2px solid white;
+    border-radius: 20px;
+    margin-top: 20px;
+    cursor: pointer;
+    background: transparent;
+    transition: background 0.3s ease;
+    z-index: 2;
+  }
+
+  button:hover {
+    background: lightgray;
+  }
+
+  @media (max-width: 768px) {
+    button {
+      font-size: 14px;
+      margin-top: 10px;
+    }
+  }
+`;
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,30 +1,35 @@
 import React from 'react';
-import { ContainerFirst, ContainerSecond, ContainerThree, ContainerFor } from './Styles';
+import { Container } from '../../components/container';
+import backgroundImg from '../../Images/background.png';
+import backgroundFisioterapiaGeral from '../../Images/FisioTerapiaGeral.png';
+import backgroundReabilitaçãoMuscular from '../../Images/ReabilitaçãoMuscular.png';
+import backgroundPilates from '../../Images/Pilates.png';
 
 export function Details() {
   return (
     <div>
-      <ContainerFirst>
+      <Container backgroundImage={backgroundImg}>
         <h2>OLÁ! SOU A DRA. JULIANA SILVA, <br/> MUITO PRAZER</h2>
         <p>APAIXONADA POR FISIOTERAPIA GERAL<br/>REABILITAÇÃO MUSCULAR<br/>TRATAMENTO DE LESÕES<br/>PILATES</p>
         <button onClick={() => window.location.href = 'https://wa.me/559183516196'}>Fale com a Doutora</button>
-      </ContainerFirst>
+      </Container>
 
-      <ContainerSecond>
+      <Container backgroundImage={backgroundFisioterapiaGeral}>
         <h2>Reabilitação Muscular: Caminho para o Bem-Estar</h2>
-        <p>A reabilitação muscular é um processo personalizado e dedicado à restauração da função muscular, visando melhorar a qualidade de vida dos pacientes. A Dra. Juliana é especializada em utilizar abordagens inovadoras, combinando terapias manuais, exercícios específicos e tecnologia avançada para promover a cura e fortalecimento muscular.</p>
-        <button onClick={() => window.location.href = 'https://wa.me/559183516196'}>Fale com a Doutora</button>
-      </ContainerSecond>
-      <ContainerThree>
+        <p>A reabilitação muscular é um processo personalizado e dedicado à restauração da função muscular, visando melhorar a qualidade de vida dos pacientes. A Dra. Juliana é especializada em utilizar abordagens inovadoras, combinando terapias manuais, exercícios específicos e tecnologia avançada para promover a cura e fortalecimento muscular.</p>        <button onClick={() => window.location.href = 'https://wa.me/559183516196'}>Fale com a Doutora</button>
+      </Container>
+
+      <Container backgroundImage={backgroundReabilitaçãoMuscular}>
         <h2>Tratamento de Lesões: Recuperação Personalizada para Sua Vitalidade</h2>
         <p>Na nossa clínica, entendemos que as lesões podem impactar profundamente a qualidade de vida. Oferecemos um tratamento dedicado e personalizado, guiando-o passo a passo em direção à recuperação completa. Nosso foco no tratamento de lesões vai além da simples cura; buscamos restaurar sua força, mobilidade e confiança.</p>
         <button onClick={() => window.location.href = 'https://wa.me/559183516196'}>Fale com a Doutora</button>
-      </ContainerThree>
-      <ContainerFor>
+      </Container>
+
+      <Container backgroundImage={backgroundPilates}>
         <h2>Descubra o Pilates: Transforme Seu Corpo, Fortaleça Sua Mente</h2>
         <p>Bem-vindo à nossa jornada de Pilates, onde a transformação física e mental se entrelaçam para criar uma nova versão mais forte, flexível e equilibrada de você mesmo. O Pilates é mais do que um exercício; é uma abordagem holística que fortalece não apenas seu corpo, mas também sua mente. Descubra como nosso programa de Pilates pode elevar sua qualidade de vida.</p>
         <button onClick={() => window.location.href = 'https://wa.me/559183516196'}>Fale com a Doutora</button>
-      </ContainerFor>
+      </Container>
     </div>
   );
 }
